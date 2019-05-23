17af2f29ba1227e133b476a5a6048332bf570d71
Dreyark <Dreyark>
2019-05-23 12:52:46 +0200

    camera follow

---
+++ ProjektInd/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform playerPos;
+    public float smoothness;
+    public Vector3 Velocity;
+    void Update()
+    {
+        Vector3 target = new Vector3(playerPos.position.x, playerPos.position.y, -10f);
+        transform.position = Vector3.SmoothDamp(transform.position, target, ref Velocity, smoothness);
+    }
+}
